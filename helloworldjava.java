b1be7cb9a5b0dcb65a4563846dd6ab63596ee971
Deepankarcloud <95834689+Deepankarcloud@users.noreply.github.com>
2021-12-18 13:27:51 +0530

    Update helloworldjava.java

--- helloworldjava.java
+++ helloworldjava.java
@@ -1,6 +1,6 @@
 public class helloworldjava {
    public static void main(String[] args) {
-    System.out.println("Hello World from DevOps-webhook tests");
+    System.out.println("Hello World from DevOps-webhook tests-dev1");
     }
 }
 
